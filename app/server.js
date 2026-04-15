33fa403352f5eeb692ef68c2164e679b81d1edbd
abhikrishu2814 <mangalabhi28@gmail.com>
2026-04-15 10:05:41 +0530

    test trigger automatoion

--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end("🚀 CI/CD Working via Jenkins + Docker");
+  res.end("🚀 CI/CD Working via Jenkins + Docker + automationpush + dockerhub");
 }).listen(3000);
